7e739aba92128529c6a5f9bc97a5ba12cd80882a
kessa99 <kessadavidkipre@gmail.com>
2023-10-17 06:32:16 -0700

    second task add

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,17 @@
+#!/bin/usr/env python3
+
+"""
+listt all documents Mongo in python
+"""
+
+
+def list_all(mongo_collection):
+    """
+    list document
+    """
+    documents = []
+    cursor = mongo_collection.find()
+
+    for document in cursor:
+        documents.append(document)
+    return documents
