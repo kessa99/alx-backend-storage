ec468b1b460a5ce8dbf821b4e2dda3973a9f7dd1
kessa99 <kessadavidkipre@gmail.com>
2023-10-20 01:26:39 -0700

    sees statistiqaues

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,21 +6,22 @@ METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
 def log_stats(mongo_collection):
+    """
+    1-connexion to database
+    2-Calcul coleection number in the database
+    3-calculs for the number of method used
+    """
 
-    total_logs = mongo_collection.count_documents({})
-    print(f"{total_logs} logs")
-
-    print("Methods:")
-    for method in METHODS:
-        count = mongo_collection.count_documents({"method": method})
-        print(f"\t{method}: {count}")
-
-    status_check = mongo_collection.count_documents(
-                   {"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
-
-
-if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
-    log_stats(nginx_collection)
+
+    num_logs = nginx_collection.count_documents({})
+    print("{} logs".format(num_logs))
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        num_method = nginx_collection.count_documents({"method": method})
+        print("\t{} {}".format(num_method, method))
+
+    num_get_status = nginx_collection.count_document({"method": "GET", "path": "/status"})
+    print("{} method = GET path=/status".format(num_get_status))
